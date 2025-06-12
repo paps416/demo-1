e7b73842c62cf89f1470426365791577c3299713
mozarte <adexplemon@gmail.com>
2025-06-12 19:14:12 +0400

    + обучение bow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data_loader import load_imdb_data
 from text_preprocessor import normalize_text
+from models import train_bow_classifier, evaluate_model
 
 if __name__ == "__main__":
     # settings
@@ -18,3 +19,8 @@ if __name__ == "__main__":
     processed_test_texts = [normalize_text(text) for text in test_texts]
     
     print("предобработка завершена")
+    
+    print("\nмодель bow")
+    bow_model, bow_vectorizer = train_bow_classifier(processed_train_texts, train_labels)
+    bow_acc, bow_f1 = evaluate_model(bow_model, bow_vectorizer, processed_test_texts, test_labels)
+    print(f"результат F1-метрики для bow: {bow_f1:.4f}")
--- models.py
+++ models.py
@@ -1,11 +1,32 @@
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score
+# import joblib
+
 def train_tfidf_classifier(texts, labels):
-    print("tf-idf")
+    print("TF-IDF in process")
     return None, None
 
 def train_bow_classifier(texts, labels):
-    print("bow")
-    return None, None
+
+    print("обучаем модель на основе bow")
+    
+    vectorizer = CountVectorizer(
+        ngram_range=(1, 2), 
+        max_features=25000
+    )
+    X = vectorizer.fit_transform(texts)
+    
+    model = LogisticRegression(random_state=42, max_iter=1000)
+    model.fit(X, labels)
+    
+    return model, vectorizer
 
 def evaluate_model(model, vectorizer, texts, labels):
-    print("rate")
-    return 0.0, 0.0
+    X_test = vectorizer.transform(texts)
+    predictions = model.predict(X_test)
+    
+    acc = accuracy_score(labels, predictions)
+    f1 = f1_score(labels, predictions, average='binary') 
+    
+    return acc, f1
