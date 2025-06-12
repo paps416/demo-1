a58c6e9e9a55536a2e22ea69931fb77b69a3361d
mozarte <adexplemon@gmail.com>
2025-06-12 18:52:33 +0400

    preprocessing

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from data_loader import load_imdb_data
+from text_preprocessor import normalize_text
 
 if __name__ == "__main__":
     # settings
-    TRAIN_SAMPLES = 1000
-    TEST_SAMPLES = 250
+    TRAIN_SAMPLES = 100
+    TEST_SAMPLES = 25
 
     print(f"загружаем {TRAIN_SAMPLES} обучающих и {TEST_SAMPLES} тестовых сэмплов")
     (train_texts, train_labels), (test_texts, test_labels) = load_imdb_data(
@@ -11,4 +12,9 @@ if __name__ == "__main__":
         test_samples=TEST_SAMPLES
     )
 
-    print("!!")
+    print("\nначинаем предобработку")
+    
+    processed_train_texts = [normalize_text(text) for text in train_texts]
+    processed_test_texts = [normalize_text(text) for text in test_texts]
+    
+    print("предобработка завершена")
--- text_preprocessor.py
+++ text_preprocessor.py
@@ -1,2 +1,27 @@
+import spacy
+# import emoji # extra
+
 def normalize_text(text):
-    return text
+    
+    # emoji to text
+    # text = emoji.demojize(text, language='alias')
+    
+    # spacy load
+    try:
+        nlp = spacy.load('en_core_web_sm') # using smallest model for speed + pc power issues
+    except OSError:
+        print("модель не найдена")
+        return "" 
+
+    # filter
+    doc = nlp(text.lower())
+    
+    # empty list
+    clean_words = []
+    
+    # working on every word
+    for token in doc:
+        if not token.is_stop and not token.is_punct and not token.is_space:
+            clean_words.append(token.lemma_)
+            
+    return " ".join(clean_words)
