b7056b1c603b9f4baf6410d931f14bb3ee2ce57d
mozarte <adexplemon@gmail.com>
2025-06-12 18:44:03 +0400

    загрузка датасета

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,22 @@
+from datasets import load_dataset
+
 def load_imdb_data(train_samples=2000, test_samples=500):
-    print("")
-    return ([], []), ([], [])
+    # (train_texts, train_labels), (test_texts, test_labels)
+    
+    print("IMBD дата сет загружается", train_samples)
+    
+    imdb_dataset = load_dataset("imdb")
+    
+    # shuffle for randomness of labels
+    train_data = imdb_dataset['train'].shuffle(seed=42).select(range(train_samples))
+    test_data = imdb_dataset['test'].shuffle(seed=42).select(range(test_samples))
+    
+    # 0 - neg, 1 - pos
+    train_texts = [item['text'] for item in train_data]
+    train_labels = [item['label'] for item in train_data]
+    
+    test_texts = [item['text'] for item in test_data]
+    test_labels = [item['label'] for item in test_data]
+    
+    print("IMDb дата сет загружен")
+    return (train_texts, train_labels), (test_texts, test_labels)
